c0a91234f121625b25357db5925c9349f5275f9a
Ushan Chamod <ushanchamodbandara@gmail.com>
2024-01-08 03:15:16 +0530

    modified:   src/Components/Admin/Admin.jsx

--- src/Components/Admin/Admin.jsx
+++ src/Components/Admin/Admin.jsx
@@ -1,21 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Admin.css'
 import Cover from '../cover/cover'
 import Midwife from '../Admin/Midwife/Midwife'
 
 export default function Admin() {
+    const[active, setActive] = useState('midwife')
     return (
         <div className='admin-container'>
             <Cover />
             <div className='navigation-container'>
                 <ul>
-                    <li>Midwife</li>
-                    <li>Medical officers</li>
-                    <li>News Feeds</li>
-                    <li>Baby Details</li>
+                    <li onClick={()=> setActive('midwife')} style={active === 'midwife' ? {background: '#fff', color: 'green', fontWeight: 'bold'}: {}}>Midwife</li>
+                    <li onClick={()=> setActive('medical_officers')} style={active === 'medical_officers' ? {background: '#fff', color: 'green', fontWeight: 'bold'}: {}}>Medical officers</li>
+                    <li onClick={()=> setActive('news')} style={active === 'news' ? {background: '#fff', color: 'green', fontWeight: 'bold'}: {}}>News Feeds</li>
+                    <li onClick={()=> setActive('baby_details')} style={active === 'baby_details' ? {background: '#fff', color: 'green', fontWeight: 'bold'}: {}}>Baby Details</li>
                 </ul>
             </div>
-            <Midwife />
+            {/* <Midwife /> */}
+            {
+                active === 'midwife' ? <Midwife /> : 
+                active === 'medical_officers' ? <p>medical_officers</p> :
+                active === 'news' ? <p>news feed</p> :
+                active === 'baby_details' ? <p>baby_details</p> : null
+            }
         </div>
     )
 }
