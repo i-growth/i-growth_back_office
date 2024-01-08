fcd29fb4602306b57661757e13ab81e7640640b9
TharinduNilan <tharindunilan04@gmail.com>
2024-01-08 19:13:46 +0530

    Changes to be committed: 	modified:   src/Components/MidwifeComponent/BabyDetails/BabyDetails.jsx

--- src/Components/MidwifeComponent/BabyDetails/BabyDetails.jsx
+++ src/Components/MidwifeComponent/BabyDetails/BabyDetails.jsx
@@ -5,15 +5,15 @@ import { AiFillCloseCircle } from 'react-icons/ai'
 export default function BabyDetails() {
 
     const data = [
-        { id: '1', no: "01", child_id: "10", childl_name: "02", age: '04', gender: '01', gurdient_name: 'father', mobile: '0123456789' },
-        { id: '1', no: "01", child_id: "10", childl_name: "02", age: '04', gender: '01', gurdient_name: 'father', mobile: '0123456789' },
-        { id: '1', no: "01", child_id: "10", childl_name: "02", age: '04', gender: '01', gurdient_name: 'father', mobile: '0123456789' },
-        { id: '1', no: "01", child_id: "10", childl_name: "02", age: '04', gender: '01', gurdient_name: 'father', mobile: '0123456789' },
-        { id: '1', no: "01", child_id: "10", childl_name: "02", age: '04', gender: '01', gurdient_name: 'father', mobile: '0123456789' },
-        { id: '1', no: "01", child_id: "10", childl_name: "02", age: '04', gender: '01', gurdient_name: 'father', mobile: '0123456789' },
-        { id: '1', no: "01", child_id: "10", childl_name: "02", age: '04', gender: '01', gurdient_name: 'father', mobile: '0123456789' },
-        { id: '1', no: "01", child_id: "10", childl_name: "02", age: '04', gender: '01', gurdient_name: 'father', mobile: '0123456789' },
-        { id: '1', no: "01", child_id: "10", childl_name: "02", age: '04', gender: '01', gurdient_name: 'father', mobile: '0123456789' },
+        { id: '1', no: "01", child_id: "10", childl_name: "01", age: '04', gender: '01', gurdient_name: 'father', mobile: '0123456789' },
+        { id: '2', no: "02", child_id: "10", childl_name: "02", age: '04', gender: '01', gurdient_name: 'father', mobile: '0123456789' },
+        { id: '3', no: "03", child_id: "10", childl_name: "03", age: '04', gender: '01', gurdient_name: 'father', mobile: '0123456789' },
+        { id: '4', no: "04", child_id: "10", childl_name: "04", age: '04', gender: '01', gurdient_name: 'father', mobile: '0123456789' },
+        { id: '5', no: "05", child_id: "10", childl_name: "05", age: '04', gender: '01', gurdient_name: 'father', mobile: '0123456789' },
+        { id: '6', no: "06", child_id: "10", childl_name: "06", age: '04', gender: '01', gurdient_name: 'father', mobile: '0123456789' },
+        { id: '7', no: "07", child_id: "10", childl_name: "07", age: '04', gender: '01', gurdient_name: 'father', mobile: '0123456789' },
+        { id: '8', no: "08", child_id: "10", childl_name: "08", age: '04', gender: '01', gurdient_name: 'father', mobile: '0123456789' },
+        { id: '9', no: "09", child_id: "10", childl_name: "09", age: '04', gender: '01', gurdient_name: 'father', mobile: '0123456789' },
     ]
 
     const [showDetail, setShowDetail] = useState(false);
@@ -70,12 +70,13 @@ export default function BabyDetails() {
                                     </div>
                                 </td>
 
-                                {showDetail && selectedBaby && selectedBaby.baby_id === data.baby_id && (
+                                {showDetail && selectedBaby && selectedBaby.no === data.no && (
                                     <div className='babyDetail-view-container'>
                                         <div className="cardView">
                                             <div className="close-icon"><AiFillCloseCircle size={25} color='red' className='icon' onClick={handleCloseViewDetail} /></div>
                                             <div className="card-section"></div>
                                             <div className="view-card-fram">
+                                                <h1>{data.childl_name}</h1>
                                             </div>
                                         </div>
                                     </div>
