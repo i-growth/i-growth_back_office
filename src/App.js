c07cc7b82f27e69652dae3560ec337658a5eb5fb
sachintha <sachintha.s.gunawardhana@gmail.com>
2024-01-08 19:10:22 +0530

    tharidus interface

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './Components/Authontication/Login';
 import Admin from './Components/Admin/Admin';
 
+
+import MidwifeComponent from './Components/MidwifeComponent/MidwifeComponent';
+
+
 function App() {
   return (
     <Router>
 
       <Routes>
         <Route path='/auth' element={<Login />} />
+
         <Route path="/" element={<Admin />} />
+
+
+     
+
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/Midwife" element={<MidwifeComponent />} />
+
       </Routes>
     </Router>
   );
