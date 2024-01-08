d7298bd8ab8e5ac555b1fdfd1a479f757c64dd30
TharinduNilan <tharindunilan04@gmail.com>
2024-01-09 04:35:19 +0530

    Changes to be committed: 	modified:   src/Components/MidwifeComponent/CreateAccount/CreateAccount.jsx 	modified:   src/Components/MidwifeComponent/CreateAccount/CreateAccount.scss 	modified:   src/Components/MidwifeComponent/MidwifeComponent.jsx 	modified:   src/Components/MidwifeComponent/MidwifeComponent.scss

--- src/Components/MidwifeComponent/CreateAccount/CreateAccount.jsx
+++ src/Components/MidwifeComponent/CreateAccount/CreateAccount.jsx
@@ -1,11 +1,122 @@
 import React from 'react'
 import './CreateAccount.scss'
+import { AiFillPlusSquare } from 'react-icons/ai'
 
 export default function CreateAccount() {
     return (
         <div className='CreateAccount-container'>
-            <div className='CreateAccount-left'></div>
-            <div className='CreateAccount-right'></div>
+            <div className='CreateAccount-left'>
+                <h3>Parent Detail</h3>
+                <div className='parent-section-top'>
+                    <input type='text' placeholder='search...' />
+                    <AiFillPlusSquare fontSize={40} className='icon' />
+                </div>
+                <div className='parent-section-bottom'>
+                    <div className='detail-section'>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <td>No</td>
+                                    <td>Parent ID</td>
+                                    <td>Name</td>
+                                    {/* <td>Address</td> */}
+                                    <td>Mobile</td>
+                                    <td>More</td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>2</td>
+                                    <td>3</td>
+                                    {/* <td>4</td> */}
+                                    <td>5</td>
+                                    <td className='crud-btn'>
+                                        <div className='top-detail'>View Detail</div>
+                                        <div className='update'>Update</div>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>1</td>
+                                    <td>2</td>
+                                    <td>3</td>
+                                    {/* <td>4</td> */}
+                                    <td>5</td>
+                                    <td className='crud-btn'>
+                                        <div className='top-detail' >View Detail</div>
+                                        <div className='update'>Update</div>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+
+                    </div>
+                </div>
+            </div>
+            <div className='CreateAccount-right'>
+                <h3>Children Detail</h3>
+                <div className='body-section'>
+                    <form >
+                        <div className="form-group">
+                            <label htmlFor="name">Parent ID:</label>
+                            <input
+                                type="text"
+                                id="parent_id"
+                                name="parent_id"
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="name">Baby's Name:</label>
+                            <input
+                                type="text"
+                                id="name"
+                                name="name"
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="birthday">Birthday:</label>
+                            <input
+                                type="date"
+                                id="birthday"
+                                name="birthday"
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="gender">Month:</label>
+                            <input
+                                type="text"
+                                id="month"
+                                name="month"
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="gender">Gender:</label>
+                            <input
+                                type="text"
+                                id="gender"
+                                name="gender"
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="gender">Registration Number:</label>
+                            <input
+                                type="text"
+                                id="registration_number"
+                                name="registration_number"
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="parentName">Parent's Name:</label>
+                            <input
+                                type="text"
+                                id="parentName"
+                                name="parentName"
+                            />
+                        </div>
+                        <button type="submit">Submit</button>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
--- src/Components/MidwifeComponent/MidwifeComponent.jsx
+++ src/Components/MidwifeComponent/MidwifeComponent.jsx
@@ -15,8 +15,8 @@ export default function MinwifeComponent() {
           <li onClick={() => setActive('measure')} style={active === 'measure' ? { background: '#fff', color: 'green', fontWeight: 'bold' } : {}}>Monthly Measure</li>
           <li onClick={() => setActive('baby_detail')} style={active === 'baby_detail' ? { background: '#fff', color: 'green', fontWeight: 'bold' } : {}}>Baby Detail</li>
           <li onClick={() => setActive('vacc_detail')} style={active === 'vacc_detail' ? { background: '#fff', color: 'green', fontWeight: 'bold' } : {}}>Vacc Detail</li>
-          <li onClick={() => setActive('consult_advices')} style={active === 'consult_advices' ? { background: '#fff', color: 'green', fontWeight: 'bold' } : {}}>Consult Advices</li>
-          <li onClick={() => setActive('view_development')} style={active === 'view_development' ? { background: '#fff', color: 'green', fontWeight: 'bold' } : {}}>View Development</li>
+          {/* <li onClick={() => setActive('consult_advices')} style={active === 'consult_advices' ? { background: '#fff', color: 'green', fontWeight: 'bold' } : {}}>Consult Advices</li> */}
+          {/* <li onClick={() => setActive('view_development')} style={active === 'view_development' ? { background: '#fff', color: 'green', fontWeight: 'bold' } : {}}>View Development</li> */}
           <li onClick={() => setActive('create_account')} style={active === 'create_account' ? { background: '#fff', color: 'green', fontWeight: 'bold' } : {}}>Create Account</li>
           <li onClick={() => setActive('news-feed')} style={active === 'news-feed' ? { background: '#fff', color: 'green', fontWeight: 'bold' } : {}}>News Feed</li>
         </ul>
@@ -26,10 +26,10 @@ export default function MinwifeComponent() {
         active === 'measure' ? <Measure /> :
           active === 'baby_detail' ? <BabyDetails /> :
             active === 'vacc_detail' ? <p>Vacc Detail</p> :
-              active === 'consult_advices' ? <p>Consult Advices</p> :
-                active === 'view_development' ? <p>View Development</p> :
-                  active === 'create_account' ? <CreateAccount /> :
-                    active === 'news-feed' ? <p>News Feed</p> : null
+              // active === 'consult_advices' ? <p>Consult Advices</p> :
+              // active === 'view_development' ? <p>View Development</p> :
+              active === 'create_account' ? <CreateAccount /> :
+                active === 'news-feed' ? <p>News Feed</p> : null
       }
     </div>
   )
