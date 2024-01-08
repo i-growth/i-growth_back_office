281ee7c3edefc2e251ef14022d6ac12865496d15
sachintha <sachintha.s.gunawardhana@gmail.com>
2024-01-08 12:33:28 +0530

    vaccine cart

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './Components/Authontication/Login';
 import Admin from './Components/Admin/Admin';
+import Vaccination_cart from './Components/Midwife/Vaccination/vaccines/Vaccine_home';
+import Navbar from'./Components/Midwife/Navbar';
 
 function App() {
   return (
@@ -9,6 +11,10 @@ function App() {
       <Routes>
         <Route path='/auth' element={<Login />} />
         <Route path="/" element={<Admin />} />
+
+
+        <Route path='/vcart' element={<Vaccination_cart/>}/>
+        <Route path='/nav' element={<Navbar/>}/>
       </Routes>
     </Router>
   );
---
+++ src/Components/Midwife/Vaccination/cart/Vaccination_cart.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Vaccination_cart.scss'
+
+export default function Vaccination_cart() {
+  return (
+    <div className='vaccin_cart'>
+        <div className='box'>
+            <h2 id='vacc_title'>Vaccine 01</h2>
+            <h3 id='vacc_month'>Month 01</h3> 
+            <div id='box'>
+                <span id='description'>This vaccine  for first month children</span>   
+            </div>
+        </div>
+    </div>
+  )
+}
