6708e012b3b36a3c481dc92276f9aa754e0c12d4
Ushan Chamod <ushanchamodbandara@gmail.com>
2024-01-09 11:04:54 +0530

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -2,18 +2,16 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './Components/Authontication/Login';
 import Admin from './Components/Admin/Admin';
 import MidwifeComponent from './Components/MidwifeComponent/MidwifeComponent';
-import NewsFeed from './Components/public/NewsFeed';
-import AddNews from './Components/public/AddNews';
 
 function App() {
   return (
     <Router>
 
       <Routes>
+        <Route path='/' element={<Login />} />
         <Route path='/auth' element={<Login />} />
         <Route path="/admin" element={<Admin />} />
         <Route path="/Midwife" element={<MidwifeComponent />} />
-        <Route path="/public" element={<AddNews />} />
       </Routes>
     </Router>
   );
