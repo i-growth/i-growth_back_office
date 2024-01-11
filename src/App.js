408d6746149b14a6c5dbc4837ef34e7ec553696b
Ushan Chamod <ushanchamodbandara@gmail.com>
2024-01-12 01:46:56 +0530

    Changes to be committed: 	deleted:    src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	deleted:    src/Components/Admin/Admin.jsx 	deleted:    src/Components/Admin/Admin.scss 	deleted:    src/Components/Admin/MedicalOfficer/MedicalOfficer.jsx 	deleted:    src/Components/Admin/MedicalOfficer/MedicalOfficer.scss 	deleted:    src/Components/Admin/MedicalOfficer/MidwifeAdd/MedicalOfficerAdd.jsx 	deleted:    src/Components/Admin/MedicalOfficer/MidwifeAdd/MedicalOfficerAdd.scss 	deleted:    src/Components/Admin/Midwife/Midwife.jsx 	deleted:    src/Components/Admin/Midwife/Midwife.scss 	deleted:    src/Components/Admin/Midwife/MidwifeAdd/MidwifeAdd.jsx 	deleted:    src/Components/Admin/Midwife/MidwifeAdd/MidwifeAdd.scss 	deleted:    src/Components/Authontication/Login.css 	deleted:    src/Components/Authontication/Login.jsx 	deleted:    src/Components/MidwifeComponent/AddNewsSection/AddMidwifeNews.jsx 	deleted:    src/Components/MidwifeComponent/AddNewsSection/AddMidwifeNews.scss 	deleted:    src/Components/MidwifeComponent/BabyDetails/BabyDetails.jsx 	deleted:    src/Components/MidwifeComponent/BabyDetails/BabyDetails.scss 	deleted:    src/Components/MidwifeComponent/CreateAccount/CreateAccount.jsx 	deleted:    src/Components/MidwifeComponent/CreateAccount/CreateAccount.scss 	deleted:    src/Components/MidwifeComponent/CreateAccount/ParentDataAdd/ParentDataAdd.jsx 	deleted:    src/Components/MidwifeComponent/CreateAccount/ParentDataAdd/ParentDataAdd.scss 	deleted:    src/Components/MidwifeComponent/MidwifeComponent.jsx 	deleted:    src/Components/MidwifeComponent/MidwifeComponent.scss 	deleted:    src/Components/MidwifeComponent/MonthlyMeasurement/MonthlyMeasurement.jsx 	deleted:    src/Components/MidwifeComponent/MonthlyMeasurement/MonthlyMeasurement.scss 	deleted:    src/Components/MidwifeComponent/Vaccine/Vaccine.jsx 	deleted:    src/Components/MidwifeComponent/Vaccine/Vaccine.scss 	deleted:    src/Components/OfficerComponent/AddOfficerNews/AddOfficerNews.jsx 	deleted:    src/Components/OfficerComponent/AddOfficerNews/AddOfficerNews.scss 	deleted:    src/Components/OfficerComponent/OfficerComponent.jsx 	deleted:    src/Components/OfficerComponent/OfficerComponent.scss 	deleted:    src/Components/cover/cover.css 	deleted:    src/Components/cover/cover.jsx 	deleted:    src/Components/public/AddNews.jsx 	deleted:    src/Components/public/NewsFeed.jsx 	deleted:    src/Components/public/addNews.scss 	deleted:    src/Components/public/news.scss 	deleted:    src/images/logo192.png 	deleted:    src/images/logo1922.png 	deleted:    src/images/logo1923.png 	deleted:    src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js 	deleted:    src/utility/AxiosInstance.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './Components/Authontication/Login';
-import Admin from './Components/Admin/Admin';
-import MidwifeComponent from './Components/MidwifeComponent/MidwifeComponent';
-import OfficerComponent from './Components/OfficerComponent/OfficerComponent';
+import React from "react";
 
 function App() {
   return (
-    <Router>
-
-      <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path='/auth' element={<Login />} />
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/Midwife" element={<MidwifeComponent />} />
-        <Route path="/medical-officer" element={<OfficerComponent />} />
-      </Routes>
-    </Router>
+    <div>
+      <h1>Hello Bro</h1>
+    </div>
   );
 }
 
--- src/utility/AxiosInstance.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
-  baseURL: 'http://localhost:3005',
-  // timeout: 30000,
-  withCredentials: true
-});
-
-export default instance;
